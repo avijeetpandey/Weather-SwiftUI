8f1e42fb086b8a1b35b5285a3f97685cee32f134
Avijeet pandey <avijeetpandey25@gmail.com>
2024-03-22 12:10:50 +0530

    Add: Initial code

---
+++ Weather-SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather-SwiftUI
+//
+//  Created by Avijeet on 22/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
